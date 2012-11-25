12c0679991f84866d32a7f301502fac99da6353b
Kar Hoo Yiu <superk888@gmail.com>
2012-11-25 18:29:22 +0100

    Commit before custom login page

--- Spring-MVC-Tutorial/src/main/java/service/security/GrantedAuthorityImpl.java
+++ Spring-MVC-Tutorial/src/main/java/service/security/GrantedAuthorityImpl.java
@@ -5,8 +5,14 @@ import org.springframework.security.core.GrantedAuthority;
 public class GrantedAuthorityImpl implements GrantedAuthority{
 	private static final long serialVersionUID = 1029928088340565343L;
 
+	private String rolename;
+	
+	public GrantedAuthorityImpl(String rolename){
+		this.rolename = rolename;
+	}
+	
 	public String getAuthority() {
-		return null;
+		return this.rolename;
 	}
 
 }
--- Spring-MVC-Tutorial/src/main/java/service/security/UserDetailsImpl.java
+++ Spring-MVC-Tutorial/src/main/java/service/security/UserDetailsImpl.java
@@ -1,46 +1,50 @@
 package service.security;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class UserDetailsImpl implements UserDetails{
 	private static final long serialVersionUID = -6509897037222767090L;
+	
+	private Collection<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+	private String password;
+	private String username;
+	
+	public UserDetailsImpl(String username, String password, Collection<GrantedAuthority> authorities){
+		this.username = username;	
+		this.password = password;
+		this.authorities = authorities;
+	}
 
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.authorities;
 	}
 
 	public String getPassword() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.password;
 	}
 
 	public String getUsername() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.username;
 	}
 
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 }
